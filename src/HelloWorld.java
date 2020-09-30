d4f77f36a73014c3667d6c976a721cf211f6c465
DongHao <695620238@qq.com>
2020-09-30 17:00:33 +0800

    demo

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+* A demo class
+*
+* @author DongHao Yang
+* @version September 2020
+*/
+public class HelloWorld {
+    /**
+    * A demo method.
+    * @param args commend line arguments (not used)
+    */
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
